83195764e0c55990815439bac40efc4192be1104
Hieu Ho <hihho@ucdavis.edu>
2020-05-22 00:53:29 -0700

    Added dataGen script to 10M data points

--- cassandraDB/schema.js
+++ cassandraDB/schema.js
@@ -1,8 +1,7 @@
 const cassandra = require('cassandra-driver');
-const csvWriter = require('csv-write-stream');
+const path = require('path');
 const fs = require('fs');
 
-const writer = csvWriter();
 const faker = require('faker');
 
 const tempClient = new cassandra.Client({ contactPoints: ['localhost'], localDataCenter: 'datacenter1', keyspace: 'system' });
@@ -41,26 +40,71 @@ const seedDB = (id) => {
   };
   return data;
 };
-
+// COPY is a command of the shell cqlsh. COPY does not exist in standard syntax
+/*
 const save = () => {
-  const query = 'COPY sauron_sdc.products_by_shop (shopID, shopName, shopDate, shopSales, shopLoc, shopURL, shopItems, productID, productName, productPrice, productShipping, productURL) FROM "./data.csv" WITH header=true AND delimiter=","';
-
-  client.execute(query);
+  const query = 'COPY sauron_sdc.products_by_shop (shopID, shopName, shopDate, shopSales, shopLoc, shopURL, shopItems, productID, productName, productPrice, productShipping, productURL) FROM ? WITH header=true AND delimiter=?';
+  const params = ['/home/hieuho/Hack Reactor/sdc/suggested-module/data.csv', ','];
+  client.execute(query, params);
   console.log('copied');
 };
+*/
 
-const dataGen = () => {
-  writer.pipe(fs.createWriteStream('data.csv'));
-  for (let i = 0; i < 10000000; i += 1) {
-    writer.write(seedDB(i));
+const start = new Date();
+const writeData = fs.createWriteStream('data.csv', {
+  flags: 'w',
+});
+writeData.write('shopID, shopName, shopDate, shopSales, shopLoc, shopURL, shopItems, productID, productName, productPrice, productShipping, productURL\n', 'utf8');
+
+const dataGen = (writer, encoding, callback) => {
+  let i = 10000000;
+  let id = 0;
+  function write() {
+    let ok = true;
+    do {
+      i -= 1;
+      id += 1;
+      const shopID = id;
+      const shopName = faker.company.companyName(0);
+      const shopDate = faker.date.recent().toString();
+      const shopSales = faker.random.number(50000);
+      const shopLoc = faker.address.state();
+      const shopURL = faker.image.avatar();
+      const shopItems = faker.random.number(1000);
+      const productID = id;
+      const productName = faker.commerce.productName();
+      const productPrice = faker.commerce.price();
+      const productShipping = faker.random.boolean() ? 'FREE Shipping' : 'Free Shipping;const Eligible';
+      const productURL = faker.image.cats();
+      const data = `${shopID}, ${shopName}, ${shopDate}, ${shopSales}, ${shopLoc}, ${shopURL}, ${shopItems}, ${productID}, ${productName}, ${productPrice}, ${productShipping}, ${productURL}\n`;
+      if (i === 0) {
+        writer.write(data, encoding, callback);
+      } else {
+        ok = writer.write(data, encoding);
+      }
+    } while (i > 0 && ok);
+    if (i > 0) {
+      writer.once('drain', write);
+    }
   }
-  writer.end();
-  console.log('done');
+  console.log(`Seeding Start at ${start.toUTCString()}`);
+  write();
 };
 
-const seed = async () => {
-  await connectAndCreate();
-  await dataGen();
-};
-// /home/hieuho/Hack Reactor/sdc/suggested-module/data.csv
-seed();
+const seed = () => connectAndCreate()
+  .then(() => {
+    dataGen(writeData, 'utf-8', () => {
+      writeData.end();
+      const ending = new Date().getTime() - start.getTime();
+      console.log(`Seeding Complete! It took: ${Math.floor(ending / 60000)}m and ${((ending % 60000) / 1000).toFixed(0)}secs`);
+    });
+  })
+  .catch((err) => console.log('seed error', err));
+
+// seed();
+
+dataGen(writeData, 'utf-8', () => {
+  writeData.end();
+  const ending = new Date().getTime() - start.getTime();
+  console.log(`Seeding Complete! It took: ${Math.floor(ending / 60000)}m and ${((ending % 60000) / 1000).toFixed(0)}secs`);
+});
