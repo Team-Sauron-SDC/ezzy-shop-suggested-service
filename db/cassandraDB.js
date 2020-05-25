b7f0f61f0e3dd158c2a3b6491afdc095f95334f7
Hieu Ho <hihho@ucdavis.edu>
2020-05-24 18:34:27 -0700

    Added new way for image to be generate

--- db/cassandraDB.js
+++ db/cassandraDB.js
@@ -11,7 +11,7 @@ const connectAndCreate = () => tempClient.connect()
   .then(() => client.connect())
   .then(() => {
     console.log('Connected to Cassandra!');
-    const createTable = 'CREATE TABLE IF NOT EXISTS sauron_sdc.products_by_shop (shopID int, shopName text, shopDate text, shopSales int, shopLoc text, shopURL text, shopItems int, productID int, productName text, productPrice text, productShipping text, productUrl text, PRIMARY KEY(shopID, productID)) WITH CLUSTERING ORDER BY (productID ASC);';
+    const createTable = 'CREATE TABLE IF NOT EXISTS sauron_sdc.products_by_shop ("shopID" int, "shopName" text, "shopDate" text, "shopSales" int, "shopLoc" text, "shopURL" text, "shopItems" int, "productID" int, "productName" text, "productPrice" text, "productShipping" text, "productURL" text, PRIMARY KEY("shopID", "productID")) WITH CLUSTERING ORDER BY ("productID" ASC);';
 
     return client.execute(createTable);
   })
@@ -20,7 +20,7 @@ const connectAndCreate = () => tempClient.connect()
 // COPY is a command of the shell cqlsh. COPY does not exist in standard syntax.
 /*
 const save = () => {
-  const query = 'COPY sauron_sdc.products_by_shop (shopID, shopName, shopDate, shopSales, shopLoc, shopURL, shopItems, productID, productName, productPrice, productShipping, productURL) FROM ? WITH header=true AND delimiter=?';
+  const query = 'COPY sauron_sdc.products_by_shop ("shopID", "shopName", "shopDate", "shopSales", "shopLoc", "shopURL", "shopItems", "productID", "productName", "productPrice", "productShipping", "productURL") FROM '/home/hieuho/Hack Reactor/sdc/suggested-module/data.csv' WITH header=true AND delimiter=',';';
   const params = ['/home/hieuho/Hack Reactor/sdc/suggested-module/data.csv', ','];
   client.execute(query, params);
   console.log('Copy to Cassandra Completed!');
@@ -32,9 +32,13 @@ const doAll = () => connectAndCreate()
   .then(() => console.log('Cassandra ready for actions!'))
   .catch((err) => console.log('Connection or Seeding Error!', err));
 
-const getShop = (id) => client.execute(`SELECT * FROM sauron_sdc.products_by_shop WHERE shopID IN (${id})`);
-const get8 = (id) => client.execute(`SELECT productName, productPrice, productShipping, productURL FROM sauron_sdc.products_by_shop WHERE shopID IN (${id})`);
-const getSuggested = (id) => client.execute(`SELECT shopName, productName, productPrice, productShipping, productURL FROM sauron_sdc.products_by_shop WHERE shopID IN (${id})`);
+doAll();
+
+const getShop = (id) => client.execute(`SELECT * FROM sauron_sdc.products_by_shop WHERE "shopID" IN (${id})`);
+
+const get8 = (id) => client.execute(`SELECT "productName", "productPrice", "productShipping", "productURL" FROM sauron_sdc.products_by_shop WHERE "shopID" IN (${id})`);
+
+const getSuggested = (id) => client.execute(`SELECT "shopName", "productName", "productPrice", "productShipping", "productURL" FROM sauron_sdc.products_by_shop WHERE "shopID" IN (${id})`);
 
 module.exports = {
   doAll,
