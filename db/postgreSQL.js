a331864a7f89e70922bddcd523a95d5c4188d7db
Hieu Ho <hihho@ucdavis.edu>
2020-05-24 17:13:37 -0700

    Added suggested end point

--- db/postgreSQL.js
+++ db/postgreSQL.js
@@ -39,9 +39,11 @@ const doAll = () => Product.sync()
 
 const getShop = (id) => Product.findAll({ where: { shopID: id } });
 const get8 = (id) => Product.findAll({ attributes: ['productName', 'productPrice', 'productShipping', 'productURL'], where: { shopID: id } });
+const getSuggested = (id) => Product.findAll({ attributes: ['shopName', 'productName', 'productPrice', 'productShipping', 'productURL'], where: { shopID: id } });
 
 module.exports = {
   doAll,
   getShop,
   get8,
+  getSuggested,
 };
